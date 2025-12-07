f982e99a3751ba40322a5b7101e2c8f769c25cae
alaminchowdhury1122 <alonealamin109@gmail.com>
2025-12-08 02:01:37 +0600

    chapter1 first upload

---
+++ hello.py
@@ -0,0 +1,23 @@
+#Ask user for name
+
+#name = input("what's your name? ")
+
+#strip use to remove extra whitespace
+#name = name.strip()
+
+#make the first latter capitle
+#name= name.title()
+#or ues can use capitilize
+
+#age = input("what's your age")
+
+#print("hello,",name,)
+#print (f"hello, {name}")
+
+
+# print(name) 
+
+
+#short from 
+name = input("What's your name? ") .strip() .title()
+print(f"hello, {name}")
